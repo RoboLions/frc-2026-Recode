0903a59497648a26771d9d87f0b5f60e9bc6b277
Gokula_35 <151260752+Codingsmthing@users.noreply.github.com>
2026-06-25 13:46:57 -0400

    Hi I am Gokula
    These are just like the foundation. I just added the files were gonna add code into and im gonna add more soon......

---
+++ src/main/java/frc/robot/subsystems/statemachines/scoring/MasterStateMachine.java
@@ -0,0 +1,11 @@
+public class MasterStateMachine {
+    
+    public static IdleState idleState = new IdleState();
+    public static IntakeState intakeState = new IntakeState();
+    public static OuttakeState outtakeState = new OuttakeState()
+    public static ShootState shootState = new ShootState();
+
+
+
+
+}
